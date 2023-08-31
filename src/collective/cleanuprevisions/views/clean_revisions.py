37b81b62e937b69146194d506fb0c0648e706cf1
MrTango <md@derico.de>
2023-08-31 20:14:24 +0300

    implement clean-revisions view

--- src/collective/cleanuprevisions/views/clean_revisions.py
+++ src/collective/cleanuprevisions/views/clean_revisions.py
@@ -3,6 +3,11 @@
 from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
+from DateTime import DateTime
+from Products.CMFCore.utils import getToolByName
+from Products.CMFEditions.utilities import dereference
+from plone.protect.interfaces import IDisableCSRFProtection
+from zope.interface import alsoProvides
 
 
 class ICleanRevisions(Interface):
@@ -12,7 +17,16 @@ class ICleanRevisions(Interface):
 @implementer(ICleanRevisions)
 class CleanRevisions(BrowserView):
     def __call__(self):
-        template = """<li class="heading" i18n:translate="">
-          Sample View
-        </li>"""
-        return template
+        alsoProvides(self.request, IDisableCSRFProtection)
+        policy = getToolByName(self.context, 'portal_purgepolicy')
+        catalog = getToolByName(self.context, 'portal_catalog')
+
+        for count, brain in enumerate(catalog()):
+            obj = brain.getObject()
+
+            # only purge old content
+            if obj.created() < (DateTime()):
+                obj, history_id = dereference(obj)
+                policy.beforeSaveHook(history_id, obj)
+                print('purged revisions of object ' + obj.absolute_url_path())
+        return "cleaning finished!"
